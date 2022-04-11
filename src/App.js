474b831e4bf6845c39a75268971805f5fc5f7a6f
Vansh Gupta <vansh.199301021@muj.manipal.edu>
2022-04-11 11:00:36 +0530

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return<div className='w-screen h-screen bg-[#f0f9ff]'>
+    <div className='fixed top-0 left-0 right-0 h-14 bg-white'>
+    
+    </div>
+  </div>;
+}
+
+export default App;
