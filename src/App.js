3d0a252434e810d79bdb5048f9fa053a08c2b06e
Vansh Gupta <vansh.199301021@muj.manipal.edu>
2022-04-11 12:08:41 +0530

    Navbar complete with css functionality left

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
 function App() {
-  return<div className='w-screen h-screen bg-[#f0f9ff]'>
-    <div className='fixed top-0 left-0 right-0 h-14 bg-white'>
-    
-    </div>
-  </div>;
+  return (
+		<div className='w-screen h-screen bg-breacout100'>
+			<div className='fixed top-0 left-0 right-0 h-fit bg-white flex flex-row justify-between'>
+				<h1 className='text-breacout900 text-5xl font-semibold px-14 py-2'>
+					BREACOUT
+				</h1>
+
+				<i class='fa-solid fa-bars text-breacout900 text-5xl font-semibold px-14 py-2	'></i>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				breacout900: "#427785",
+				breacout100: "#f0f9ff",
+			},
+		},
 	},
 	plugins: [],
 };
